40ff43b1b16014c09d68b477fecc1eb6e7bc3f71
bhargavak <19756773+kbhagi@users.noreply.github.com>
2020-03-15 11:00:03 +0530

    Add files via upload

---
+++ SumNumberBetween1To100/SumNumberBetween1To100/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SumNumberBetween1To100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int num = 100;
+            int i = 1;
+            do
+            {
+                if ((i % 2 == 0))
+                    sum += i;
+                i++;
+            } while (i <= num);
+
+            Console.WriteLine("Sum of even numbers from 1 to {0} is : {1}",num,sum);
+            Console.ReadLine();
+        }
+    }
+}
